5fbd6240b4e5cfd51d35d86bdeb97aa711f0e8e3
angelotheman <ktwum721@gmail.com>
2022-05-16 20:28:17 +0000

    Changes to the bikesharedate.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -1,6 +1,7 @@
 import time
 import pandas as pd
 
+# This is a comment for refactoring branch
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
